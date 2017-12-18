6bbad210965282dea117925de8c89af316edc8fd
Rowell Belen <developer@bytekast.com>
2017-12-18 08:57:35 -0700

    Initial Commit

---
+++ generatedsources/com/bytekast/netsuite/client/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:accounting_2017_1.lists.webservices.netsuite.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.bytekast.netsuite.client;
